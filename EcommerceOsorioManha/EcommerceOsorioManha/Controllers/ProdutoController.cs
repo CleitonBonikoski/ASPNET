0cbafc525a39df9c8d48329a19d01a3b65e205fd
Aluno <Aluno@DOSO121737>
2018-07-31 09:04:33 -0300

    Aula 01

---
+++ EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoController.cs
@@ -0,0 +1,45 @@
+﻿using EcommerceOsorioManha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerceOsorioManha.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        Contexto contexto = new Contexto();
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ViewBag.Data = DateTime.Now;
+            ViewBag.Produtos = contexto.Produtos.ToList();
+            return View();
+        }
+        public ActionResult CadastrarProduto()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CadastrarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria)
+        {
+            Produto produto = new Produto()
+            {
+                Nome = txtNome,
+                Descricao = txtDescricao,
+                Preco = Convert.ToDouble(txtPreco),
+                Categoria = txtCategoria
+            };
+
+            contexto.Produtos.Add(produto);
+            contexto.SaveChanges();
+            return RedirectToAction("Index","Produto");
+
+        }
+
+
+
+    }
+}
---
+++ EcommerceOsorioManha/EcommerceOsorioManha/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceOsorioManha.Models
+{
+    public class Contexto : DbContext
+    {
+        // Nome do Banco
+        public Contexto() : base("DbEcommerce") { }
+
+        // Mapear as classes no banco
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
