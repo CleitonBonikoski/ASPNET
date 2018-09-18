e5082e79fbdf4d910103f57be4bb28d13d121eba
cleiton <cleitonbonikoski57@gmail.com>
2018-09-18 08:52:34 -0300

    Criando API

---
+++ EcommerceOsorioManha/EcommerceOsorioManha/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace EcommerceOsorioManha
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+			// Mudar formato da entrega da webapi
+			//De XML para Json
+			config.Formatters.Remove(config.Formatters.XmlFormatter);
+			config.Formatters.JsonFormatter.Indent = true;
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoAPIController.cs
@@ -0,0 +1,21 @@
+﻿using EcommerceOsorioManha.DAL;
+using EcommerceOsorioManha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EcommerceOsorioManha.Controllers
+{
+	[RoutePrefix("api/Produto")]
+    public class ProdutoAPIController : ApiController
+    {
+		[Route("Produtos")]
+		public List<Produto> GetProdutos()
+		{
+			return ProdutoDAO.RetornarProdutos();
+		}
+    }
+}
