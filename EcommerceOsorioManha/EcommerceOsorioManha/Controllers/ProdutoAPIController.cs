83037685c04971a4d31c4ad06abb5aa9e70d0492
cleiton <cleitonbonikoski57@gmail.com>
2018-09-18 09:33:11 -0300

    Adicionando comandos Api

--- EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoAPIController.cs
+++ EcommerceOsorioManha/EcommerceOsorioManha/Controllers/ProdutoAPIController.cs
@@ -10,12 +10,41 @@ using System.Web.Http;
 namespace EcommerceOsorioManha.Controllers
 {
 	[RoutePrefix("api/Produto")]
-    public class ProdutoAPIController : ApiController
-    {
+	public class ProdutoAPIController : ApiController
+	{
+		//Get: api/Produto/Produtos
 		[Route("Produtos")]
 		public List<Produto> GetProdutos()
 		{
 			return ProdutoDAO.RetornarProdutos();
 		}
-    }
+
+		//Get: api/Produto/ProdutosPorCategoria/comida
+		[Route("ProdutosPorCategoria/{CategoriaId}")]
+		public List<Produto> GetProdutosPorCategoria(string CategoriaId)
+		{
+			return ProdutoDAO.RetornarProdutosPorCategoria(CategoriaId);
+		}
+
+		//Get: api/Produto/ProdutosPorId/2
+		[Route("ProdutosPorId/{ProdutoId}")]
+		public dynamic GetProdutosPorId(int ProdutoId)
+		{
+			Produto produto = ProdutoDAO.BuscarProdutoPorId(ProdutoId);
+			List<Categoria> categorias = CategoriaDAO.RetornarCategorias();
+			if (produto != null)
+			{
+				dynamic produtoDynamic = new
+				{
+					Nome = produto.Nome,
+					Preco = produto.Preco.ToString("C2"),
+					Categorias = categorias,
+					DataEnvio = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+				};
+				return new { produto = produtoDynamic };
+			}
+
+			return NotFound();
+		}
+	}
 }
